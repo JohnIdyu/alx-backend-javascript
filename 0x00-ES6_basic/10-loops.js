f4bd58c2c64f313b31fa14afa264f1cc30aca399
JohnIdyu <idyujohncrypto@gmail.com>
2023-02-08 16:09:15 +0100

    Loops

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,1 +1,8 @@
-test
+export default function appendToEachArrayValue(array, appendString) {
+    const newArray = [];
+    for (const value of array) {
+      newArray.push(appendString + value);
+    }
+  
+    return newArray;
+  }

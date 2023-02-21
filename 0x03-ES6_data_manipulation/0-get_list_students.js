ef06d48dff3bf426608926ae20f92b2379eefaec
JohnIdyu <idyujohncrypto@gmail.com>
2023-02-21 11:41:27 +0100

    Update 0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,16 +1,7 @@
 export default function getListStudents() {
-    const o1 = {
-      id: 1,
-      firstName: 'Guillaume',
-      location: 'San Francisco',
-    };
-  
-    const o2 = {
-      id: 2,
-      firstName: 'James',
-      location: 'Columbia',
-    };
-  
-    const o3 = {
-      id: 5,
-      firstNa
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
